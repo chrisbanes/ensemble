4059ea06ba582093a3a5bc8ecefa3028ad9d8cb2
Chris Banes <chris@banes.me>
2026-09-15 19:45:39 +0100

    Fix DOM matcher types for Vitest 5

--- crates/ensemble-ui/src-ui/src/test/setup.ts
+++ crates/ensemble-ui/src-ui/src/test/setup.ts
@@ -1,4 +1,8 @@
 import { expect } from "vitest";
 import * as matchers from "@testing-library/jest-dom/matchers";
 
+declare module "vitest" {
+  interface Matchers<R, T> extends matchers.TestingLibraryMatchers<T, R> {}
+}
+
 expect.extend(matchers);
